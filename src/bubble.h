b33586461908885e4d975ad1fe2f5d52203aeb5d
Nathan <48669774+Zexxerd@users.noreply.github.com>
2021-01-08 16:06:30 -0500

    Update bubble.h

--- src/bubble.h
+++ src/bubble.h
@@ -5,21 +5,37 @@
 
 #include <graphx.h>
 #include "gfx/bubble.h"
+
+//Constants
 #define TILE_WIDTH 16
 #define TILE_HEIGHT 16
-#define MAX_COLOR 6
 #define ROW_HEIGHT ((TILE_HEIGHT>>1)+(TILE_HEIGHT>>2)) // 3/4 of the tile height
+#define MAX_COLOR 6
+
+//degrees to radians
 #define deg(a) (a * (180 / M_PI))
 #define rad(a) (a * (M_PI / 180))
+
 //tiles
 #define PROCESSED (1<<0)
 #define REMOVED   (1<<1)
 #define EMPTY     (1<<2)
+
 //shooter
 #define ACTIVE_PROJ (1<<0)
 #define DEACTIVATED (1<<1)
+
+//debug macros
+#define gfx_PrintUIntXY(i,length,x,y) gfx_SetTextXY(x,y);\
+gfx_PrintUInt(i,length)
 #define gfx_PrintIntXY(i,length,x,y) gfx_SetTextXY(x,y);\
 gfx_PrintInt(i,length)
+#define debug_message(message) \
+gfx_PrintStringXY(message,0,116); \
+gfx_BlitBuffer(); \
+while (!os_GetCSC())
+#define debug_flag() debug_flag = true; //trashy, I know...
+
 /*enum * bubble_colors {
     red = 0x7c00,
     orange = 0x7e00,
@@ -76,11 +92,10 @@ point_t getGridPosition(int x,int y);
 void init_grid(grid_t * grid,uint8_t rows,uint8_t cols,uint8_t empty_row_start);
 void renderGrid(grid_t * grid);
 void renderShooter(shooter_t * shooter);
-bool collide(double x1,double y1,double x2,double y2,uint8_t r);
+bool collide(float x1,float y1,float x2,float y2,uint8_t r);
 void snapBubble(projectile_t * projectile,grid_t * grid);
-void move_proj(grid_t * grid,shooter_t * shooter,uint8_t dt);
+void move_proj(grid_t * grid,shooter_t * shooter,float dt);
 void resetProcessed(grid_t * grid);
 bubble_list_t * getNeighbors(grid_t * grid,bubble_t tile);
 bubble_list_t * findCluster(grid_t * grid,uint8_t tile_x,uint8_t tile_y,bool matchtype,bool reset,bool skipremoved);
 bubble_list_t * findFloatingClusters(grid_t * grid);
-#undef gfx_PrintIntXY
